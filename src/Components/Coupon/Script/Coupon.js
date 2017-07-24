c9fe53227e362a6abb7015b8959abd742b39635a
LessTong <tongxinge108@163.com>
2017-07-24 22:41:40 +0800

    ss

--- src/Components/Coupon/Script/Coupon.js
+++ src/Components/Coupon/Script/Coupon.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
+import {
+	Table,
+	Popconfirm,
+	Button
+} from 'antd';
 export default class Coupon extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return (<div>aaaaaaaa</div>);
+		return (<div>
+			<Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
+		</div>);
 	}
 }
