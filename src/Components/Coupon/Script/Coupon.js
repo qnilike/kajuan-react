b047f3467069966e6c8ee582830881084b9be4a8
LessTong <tongxinge108@163.com>
2017-07-25 22:54:34 +0800

    lx

--- src/Components/Coupon/Script/Coupon.js
+++ src/Components/Coupon/Script/Coupon.js
@@ -1,46 +1,77 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { Table } from 'antd'
+import {
+  Table
+} from 'antd'
 import '../Less/style'
+import Coupon_DATA from './Coupon_DATA'
 
-const dataSource = [{
-  key: '1',
-  name: '胡彦斌',
-  age: 32,
-  address: '西湖区湖底公园1号'
-}, {
-  key: '2',
-  name: '胡彦祖',
-  age: 42,
-  address: '西湖区湖底公园1号'
-}];
+function dataSource() {
+  var a = [],
+    obj = {};
+  for (var i = 0; i < 11; i++) {
+    obj = {
+      key: i,
+      name: '胡彦斌' + i,
+      age: 32,
+      address: '西湖区湖底公园1号'
 
-const columns = [{
-  title: '姓名',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-  key: 'age',
-}, {
-  title: '住址',
-  dataIndex: 'address',
-  key: 'address',
-}];
+    }
+    a.push(obj)
+  }
+  return a;
+}
+
+var dataSource = dataSource();
 
 export default class Coupon extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      'dataSource': []
+    }
+  }
 
-	render() {
-		return (
-			<div className="content">
+
+  componentDidMount() {
+    let {
+      dataSource
+    } = this.state;
+    //ajax
+
+    //封装的地方
+    // $.ajax({
+    //   url:'',
+    //   success:function(){
+
+    //   }
+    // })
+
+
+    //GETPOST.getCo
+
+
+    // (dataSource) => {
+
+    // }
+    // this.setState({
+    //   dataSource: dataSource;
+    // })
+
+    console.log(dataSource)
+  }
+
+  render() {
+    let {
+      dataSource
+    } = this.state;
+    return (
+      <div className="content">
 				<div className="coupon">
-					<Table dataSource={dataSource} columns={columns} />	
+					<Table dataSource={dataSource} columns={Coupon_DATA} />	
 				</div>
 			</div>
-		);
-	}
+    );
+  }
 }
---
+++ src/Components/Coupon/Script/Coupon_DATA.js
@@ -0,0 +1,15 @@
+const Coupon_DATA = [{
+	title: '姓名',
+	dataIndex: 'name',
+	key: 'name',
+}, {
+	title: '年龄',
+	dataIndex: 'age',
+	key: 'age',
+}, {
+	title: '住址',
+	dataIndex: 'address',
+	key: 'address',
+}];
+
+export default Coupon_DATA;
